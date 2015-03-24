9dead60681806b698d9cb6d961fcc466cbd2f9a3
zepinglai <zepinglai@gmail.com>
2015-03-24 20:41:57 +0800

    简单统计Nginx日志IP数量

---
+++ log_ip_count.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#encoding:utf8
+#author: linuxhub.org
+#统计nginx日志IP数统计
+
+import re,sys
+
+data_file = 'D:/script/python/other/20150312_www.kuaiyouxi.com_5.log'
+
+
+#读文件数据  
+
+def ip_count(data_file):
+              with open(data_file, 'r') as f:
+                            #print f.readlines() #每次读一行
+                            count = {}
+                            for line in f.readlines():
+                                          content = line.strip() # 把末尾的'\n'删掉
+                                          log = content.split('- -')
+                                          ip = log[0]
+                                          count[ip] = count.get(ip,0)+1  #加1
+
+                            return count
+
+
+
+
+ip_count = ip_count(data_file)
+
+print 'IPAddress\t\tcount'
+for ip,count in ip_count.iteritems():
+              print '%s\t%s'%(ip,count)
+
+
+
